efeada675ebee52fc455e6242e35de1ba1dd7e0f
Martin Dvořák <martin.d@outlook.cz>
2025-07-18 15:25:31 +0200

    Define entities and index

--- Contexts/CurrencyDbContext.cs
+++ Contexts/CurrencyDbContext.cs
@@ -12,6 +12,32 @@ namespace Btc.Api.Contexts
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BitcoinRateRecord>()
+                .Property(x => x.BtcEur)
+                .HasPrecision(18, 6);
+
+            modelBuilder.Entity<BitcoinRateRecord>()
+                .Property(x => x.DailyChangePercent)
+                .HasPrecision(18, 6);
+
+            modelBuilder.Entity<BitcoinRateRecord>()
+                .Property(x => x.EurCzk)
+                .HasPrecision(18, 6);
+
+            modelBuilder.Entity<CnbCurrencyRate>()
+                .Property(r => r.Rate)
+                .HasPrecision(18, 6);
+
+            modelBuilder.Entity<CnbCurrencyRate>()
+               .HasIndex(x => new { x.CurrencyCode, x.ValidFor })
+               .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 
 }
